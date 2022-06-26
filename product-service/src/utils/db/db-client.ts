9b9ba5ebd308259e3aeb6123928e9101004efea7
nikita-resh <nikita.reshetnikov9@gmail.com>
2022-06-26 20:16:44 +0400

    feat: add pg client

---
+++ product-service/src/utils/db/db-client.ts
@@ -0,0 +1,19 @@
+import { Client } from 'pg';
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const dbOptions = {
+	host: PG_HOST,
+	port: PG_PORT,
+	database: PG_DATABASE,
+	user: PG_USERNAME,
+	password: PG_PASSWORD,
+	ssl: {
+		rejectUnauthorized: false
+	},
+	connectionTimeoutMillis: 5000
+};
+
+export const getClient = () => {
+	return new Client(dbOptions);
+};
