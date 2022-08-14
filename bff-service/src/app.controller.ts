3cb5c7bc84bf72edeea6859967fece1485989863
nikita-resh <nikita.reshetnikov9@gmail.com>
2022-08-14 11:54:16 +0400

    feat: implemented BFF

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Param, HttpException, HttpStatus, Query, All, Req } from '@nestjs/common';
+import axios from 'axios';
+import { Request } from 'express';
+
+const isServiceExists = (serviceName: string) => {
+  return Boolean(process.env[serviceName]);
+};
+
+const isEmptyObject = (obj: Object) => {
+  return Object.keys(obj).length === 0;
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @All('/:service')
+  async serviceGet(
+    @Param('service') service: string,
+    @Query('id') id: string,
+    @Req() req: Request,
+  ): Promise<any> {
+    if (!isServiceExists(service)) {
+      throw new HttpException(`Cannot process request`, HttpStatus.BAD_GATEWAY);
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    try {
+      const response = await axios.request({
+        method: req.method,
+        url: process.env[service] + (id ? `/${id}` : ''),
+        data: isEmptyObject(req.body) ? null : req.body,
+      });
+
+      return response.data;
+    } catch (err: any) {
+      console.log(err);
+      throw new HttpException(err.response.data.message, err.response.status);
+    }
   }
 }
