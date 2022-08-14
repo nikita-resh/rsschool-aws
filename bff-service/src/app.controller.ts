b4cf17b994585e78d1cd2dde251161917e681ad5
nikita-resh <nikita.reshetnikov9@gmail.com>
2022-08-14 12:29:15 +0400

    feat: added in-memory caching

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Param, HttpException, HttpStatus, Query, All, Req } from '@nestjs/common';
+import {
+  All,
+  CACHE_MANAGER,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  Query,
+  Req,
+  Request,
+} from '@nestjs/common';
 import axios from 'axios';
-import { Request } from 'express';
+import { Cache } from 'cache-manager';
 
 const isServiceExists = (serviceName: string) => {
   return Boolean(process.env[serviceName]);
@@ -12,6 +23,8 @@ const isEmptyObject = (obj: Object) => {
 
 @Controller()
 export class AppController {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
   @All('/:service')
   async serviceGet(
     @Param('service') service: string,
@@ -22,12 +35,25 @@ export class AppController {
       throw new HttpException(`Cannot process request`, HttpStatus.BAD_GATEWAY);
     }
 
+    const isGetProductsRequest = req.method === 'GET' && req.url.includes('product');
+
     try {
+      if (isGetProductsRequest) {
+        const cachedValue = await this.cacheManager.get(req.url);
+        if (cachedValue) {
+          return cachedValue;
+        }
+      }
+
       const response = await axios.request({
         method: req.method,
         url: process.env[service] + (id ? `/${id}` : ''),
         data: isEmptyObject(req.body) ? null : req.body,
       });
+
+      if (isGetProductsRequest) {
+        await this.cacheManager.set(req.url, response.data);
+      }
 
       return response.data;
     } catch (err: any) {
--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+
+const MINUTE = 60;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.development.env',
+    }),
+    CacheModule.register({
+      ttl: 2 * MINUTE,
     }),
   ],
   controllers: [AppController],
